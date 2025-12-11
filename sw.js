666b99c3247032a344dc3aa3f0491ba7462c8d89
Osretna <127016877+Osretna@users.noreply.github.com>
2025-12-11 06:11:54 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = 'mystore-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/main.js',
+  './js/firebase-config.js'
+];
 
+// تثبيت التطبيق
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// تشغيل التطبيق (جلب الملفات)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
